f289d5938583eb013d923ca54dd2ff9b2c364755
romelgomez <bmxandcode@gmail.com>
2024-03-09 20:51:01 -0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by romel javier gomez herrera on 9/03/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
